8ee682cb2792e8d18e051fea1b5dcdf9ac74c76d
jmlascano <jmlascano@up.edu.ph>
2025-05-27 17:43:50 +0800

    feat: Add edit API to routes

--- backend/controllers/memberFee.controller.js
+++ backend/controllers/memberFee.controller.js
@@ -14,3 +14,32 @@ export const getAllMembers = async (req, res) => {
     return res.status(500).json({ error: 'Internal server error' });
   }
 }
+
+// Edit Member Details
+export const editMember = async (req, res) => {
+  try {
+    const { memberId } = req.params;
+    const { member_username, member_name, gender, degree_program } = req.body;
+    
+    if (!member_name || !member_username || !gender || !degree_program) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    const updateQuery = `
+      UPDATE member
+      SET member_name = ?, member_username = ?, gender = ?, degree_program = ?
+      WHERE memmber_id = ?
+    `;
+
+    await db.query(updateQuery, [member_name, member_username, gender, degree_program, memberId]);
+
+    return res.status(201).json({
+      success: true,
+      message: 'Edit of member success'
+    });
+
+  } catch (error) {
+    console.error('Edit member error:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
--- backend/routers/orgMember.router.js
+++ backend/routers/orgMember.router.js
@@ -1,4 +1,4 @@
-import { getAllOrgs, searchAndFilterMembers, addMemberToOrg, updateMemberOrgStatus, removeMemberFromOrg, getOrgMemberStats, getOrgEvents } from "../controllers/orgMember.controller.js";
+import { getAllOrgs, searchAndFilterMembers, addMemberToOrg, updateMemberOrgStatus, removeMemberFromOrg, getOrgMemberStats, getOrgEvent, editOrg } from "../controllers/orgMember.controller.js";
 
 export const orgMemberRouter = (app) => {
   app.get("/orgs", getAllOrgs);
@@ -8,4 +8,5 @@ export const orgMemberRouter = (app) => {
   app.delete("/orgs/:orgId/members/:memberId/:acadYear/:acadSem", removeMemberFromOrg);
   app.get("/orgs/:orgId/members/stats", getOrgMemberStats);
   app.get("/orgs/:orgId/events", getOrgEvents);
+  app.post("/orgs/:orgId", editOrg);
 }
